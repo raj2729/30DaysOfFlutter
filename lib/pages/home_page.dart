0e234252272c2c5c29d6b0a02cd6efe76855a66b
Raj <rajsanghavi9@gmail.com>
2021-05-25 17:23:48 +0530

    day12 completed

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_catalogue/models/catalogue.dart';
 import 'package:flutter_catalogue/widgets/drawer.dart';
+import 'package:flutter_catalogue/widgets/item_widget.dart';
 
 class HomePage extends StatelessWidget {
   HomePage({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final dummyList = List.generate(20, (index) => CatalogModel.items[0]);
 
     // Scaffold => a lot of built in components
     // Name of components = name of propety
@@ -15,12 +18,20 @@ class HomePage extends StatelessWidget {
         // backgroundColor: Colors.white,
         // elevation: 0.0,
         // iconTheme: IconThemeData(color: Colors.black),
-        title: Text('Raj Application',
-         ),
+        title: Text(
+          'Raj Application',
+        ),
       ),
-      body: Center(
-        child: Container(
-          child: Text('Welcome to HomePage'),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: ListView.builder(
+          // itemCount: CatalogModel.items.length,
+          itemCount: dummyList.length,
+          itemBuilder: (context, index) {
+            // index is position i.e start of array
+            // return ItemWidget(item: CatalogModel.items[index] ,);
+            return ItemWidget(item: dummyList[index]);
+          },
         ),
       ),
       drawer: MyDrawer(),
---
+++ lib/widgets/item_widget.dart
@@ -0,0 +1,40 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_catalogue/models/catalogue.dart';
+
+class ItemWidget extends StatelessWidget {
+  final Item item;
+
+  const ItemWidget({Key key, @required this.item})
+      : assert(item != null),
+        super(key: key);
+
+  // Assert so that item passed is not null
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Card(
+      elevation: 0,
+      child: ListTile(
+        onTap: () {
+          print("${item.name}");
+        },
+        leading: Image.network(
+          item.image,
+        ),
+        title: Text("Iphone 12 Max Pro"),
+        subtitle: Text(item.description),
+        trailing: Text(
+          "Rs ${item.price}",
+          textScaleFactor: 1.2,
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            color: Colors.deepPurple,
+          ),
+        ),
+      ),
+    );
+  }
+}
