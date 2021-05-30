5c7e561eb1c7dd6721a77d25411e40cf0407b39e
Raj <rajsanghavi9@gmail.com>
2021-05-30 14:09:47 +0530

    day13 completed

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,14 +1,42 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+// To decode json files
+import 'dart:convert';
 import 'package:flutter_catalogue/models/catalogue.dart';
 import 'package:flutter_catalogue/widgets/drawer.dart';
 import 'package:flutter_catalogue/widgets/item_widget.dart';
 
-class HomePage extends StatelessWidget {
+// Stateless => no changes
+// While importing data => we need to change data
+// => Stateful
+class HomePage extends StatefulWidget {
   HomePage({Key key}) : super(key: key);
 
   @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    loadData();
+  }
+
+  loadData() async {
+    final catalogJson =
+        await rootBundle.loadString("assets/files/catalog.json");
+    // print(catalogJson);
+    //  To decode dtaa of json
+    final decodedData = jsonDecode("assets/files/catalog.json");
+    // Gives entire json, we want only products
+    var products = decodedData["products"];
+  }
+
+  @override
   Widget build(BuildContext context) {
-    final dummyList = List.generate(20, (index) => CatalogModel.items[0]);
+    final dummyList = List.generate(4, (index) => CatalogModel.items[0]);
 
     // Scaffold => a lot of built in components
     // Name of components = name of propety
